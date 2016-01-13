c1d4b35e0c630d8c1b95c578ecfcbabdae55c48b
Nic Netzel and torrence stratton <netzeln@gmail.com>
2016-01-13 13:58:39 -0800

    make a list from looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
+  $("button").click(function() {
+    var flavors = ["cinnamon", "coffee", "Coconut", "Toasted Almond Fudge", "Mint", "Rum Cherry", "Pistachio", "Caramel Swirl"];
+    flavors.forEach(function(flavor) {
+//      $(".flavor-list").attr("<li>").text(flavor);
+      $("ul").append("<li>" + flavor + "");
 
+    });
+  });
 
 
 });
